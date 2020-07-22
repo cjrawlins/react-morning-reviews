1014ffe67225a16a0d4934b51d1943252ac7e91b
cjrawlins <caseyrawlins@gmail.com>
2020-07-22 10:36:36 -0600

    end of review1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import List from "./Components/List.js"
+import './App.css';
 
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="movie-list-title">THE MOST AMAZING MOVIE LIST</h1>
+            <List className="List"/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: []
+        }
+    }
+  
+    render() {
+        return (
+            <h1>This is the List Component</h1>
+        )
+    }
+}
+
+export default List;
